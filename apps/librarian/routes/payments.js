35497a0ae2c0e6db1cabc6400f619eae9595c573
Dillon Fagan <dillonfagan@pm.me>
2023-11-18 11:49:31 -0500

    Add endpoint to fetch payments

--- apps/librarian/routes/payments.js
+++ apps/librarian/routes/payments.js
@@ -1,7 +1,18 @@
-const { recordCashPayment } = require('../../../services/payments');
+const { fetchPayments, recordCashPayment } = require('../../../services/payments/payments');
 
 const express = require('express');
 const router = express.Router();
+
+router.get('/:borrowerId', async (req, res) => {
+    const { borrowerId } = req.params;
+    try {
+        const payments = await fetchPayments(borrowerId);
+        res.status(200).send(payments);
+    } catch (error) {
+        console.error(error);
+        res.status(error.statusCode || 500).send({ error: error.toString() });
+    }
+});
 
 router.put('/:borrowerId', async (req, res) => {
     const { borrowerId } = req.params;

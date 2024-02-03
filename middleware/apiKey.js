306e29899292d198d4bcac77b1fb6445375d7c2e
Dillon Fagan <dillonfagan@pm.me>
2024-02-03 13:02:45 -0500

    Handle empty API_KEY var

--- middleware/apiKey.js
+++ middleware/apiKey.js
@@ -2,7 +2,7 @@ const authorize = async (req, res, next) => {
   const apiKey = process.env.API_KEY;
   const apiKeyHeader = req.headers['x-api-key'];
 
-  if (apiKeyHeader !== apiKey) {
+  if (apiKey && apiKeyHeader !== apiKey) {
     res.status(403).send();
     return;
   }
